fef521af2421075b872fac9b33ee55c7c080cc3b
Pajar0p <villarroel.cvd@gmail.com>
2013-07-30 20:52:39 +0000

    index

--- web.js
+++ web.js
@@ -1,10 +1,15 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
 
+var data = fs.readFileSync("index.html","utf-8");
+
+var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-  response.send('Hello World 2: Pablo vale caca!');
+        response.send(data);
+
 });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
