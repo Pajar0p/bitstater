b310d99ccde390f1e0e4095b5b3a16aa28d8d5b1
Pajar0p <villarroel.cvd@gmail.com>
2013-08-06 18:02:31 +0000

    bootstrap en web-js

--- web.js
+++ web.js
@@ -7,11 +7,11 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
         response.send(data);
 
+
 });
 
-
-
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
+app.use(express.static(__dirname + '/bootstrap'));
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
